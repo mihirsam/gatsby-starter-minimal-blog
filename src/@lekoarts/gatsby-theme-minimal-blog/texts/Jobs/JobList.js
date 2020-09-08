4c970421b94e3f501e0584bd9e67b8145bc42c03
Mihir <xenon.of.x@gmail.com>
2020-09-08 16:21:57 +0530

    add new logo complete hero text and work experiance

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/texts/Jobs/JobList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function JobList() {
+    return (
+        <div>
+            <h1>
+                Software Developer
+                <span style={{ color: "red" }}>
+                    @ Nomura Research Institute Fintech
+                </span>
+            </h1>
+            <p style={{ "font-size": "20px" }}>August, 2020 - Present</p>
+            <br />
+        </div>
+    );
+}
+
+export default JobList;
