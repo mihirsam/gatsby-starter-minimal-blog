30f34e1b21689927dbf52a0f6d61049e9bd92a80
Mihir <xenon.of.x@gmail.com>
2020-09-12 03:26:09 +0530

    complete hero project part

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/texts/Projects/ProList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ProList() {
+    return (
+        <div>
+            <div>
+                <h4>Information-Extraction-using-CNN</h4>
+                <span>
+                    <p style={{ "font-size": "20px" }}>
+                        Jan - July, 2020 &nbsp; &bull; &nbsp; 7 Months
+                    </p>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default ProList;
